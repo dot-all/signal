34fa393c1fe1275420455f693730877931d120f6
Allan <al.quirozsilva@gmail.com>
2026-04-18 17:22:57 -0400

    feat: initialize Spring Boot project with Maven wrapper and core dependencies

---
+++ src/test/java/cl/allan/signal/SignalApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.allan.signal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
